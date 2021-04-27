f127ef2c4de2d73c717859df02f4005486072b49
LautaroCoccia <lautaroauditore@gmail.com>
2021-04-27 16:18:29 -0300

    Update ObjectPooler.cs

--- Assets/GameAssets/Scripts/ObjectPooler.cs
+++ Assets/GameAssets/Scripts/ObjectPooler.cs
@@ -6,6 +6,7 @@ public class ObjectPooler : MonoBehaviour
     [System.Serializable]
     public class Pool
     {
+        public float health;
         public string tag;
         public GameObject prefab;
         public int size;
@@ -13,7 +14,6 @@ public class ObjectPooler : MonoBehaviour
         public float timeToSpawn = 20;
         public float RandomRangePosition;
         public float actualTime = 0;
-        public int algo = 0;
     }
     #region Singleton
     private static ObjectPooler Instance;
@@ -45,8 +45,7 @@ public class ObjectPooler : MonoBehaviour
             {
                 GameObject obj = Instantiate(pool.prefab);
                 obj.SetActive(false);
-                MeshRenderer  mesh =obj.GetComponent<MeshRenderer>();
-                //mesh.enabled = false;
+                obj.transform.gameObject.GetComponent<IPoolValuesInitializer>().SetHealth(pool.health);
                 objectPool.Enqueue(obj);
             }
             poolDictionary.Add(pool.tag, objectPool);
@@ -65,10 +64,10 @@ public class ObjectPooler : MonoBehaviour
                 else if(pool.actualTime >= pool.timeToSpawn && pool.actualAlive < pool.size)
                 {
                     SpawnFromPool(pool.tag, new Vector3(Random.Range(-pool.RandomRangePosition, pool.RandomRangePosition), 1, Random.Range(-pool.RandomRangePosition, pool.RandomRangePosition)), Quaternion.identity);
+
+                    
                     pool.actualTime = 0;
                     pool.actualAlive++;
-                    pool.algo++;
-                    Debug.Log(pool.algo);
                 }
             }
         }
@@ -80,9 +79,6 @@ public class ObjectPooler : MonoBehaviour
             Debug.LogWarning("Pool with tag " + tag + " doesn't excist");
         }
         GameObject objectToSpawn = poolDictionary[tag].Dequeue();
-        MeshRenderer mesh = objectToSpawn.GetComponent<MeshRenderer>();
-        
-        //mesh.enabled = true;
         objectToSpawn.transform.position = position;
         objectToSpawn.transform.rotation = rotation;
         objectToSpawn.SetActive(true);
@@ -92,5 +88,5 @@ public class ObjectPooler : MonoBehaviour
             pooledObj.OnObjectSpawn();
         }
         poolDictionary[tag].Enqueue(objectToSpawn);
-    } 
+    }
 }
