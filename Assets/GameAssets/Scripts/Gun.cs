294166d49a494af3ee84d1a6b7438c07223ce944
LautaroCoccia <lautaroauditore@gmail.com>
2021-04-27 16:14:58 -0300

    Add Gun script

---
+++ Assets/GameAssets/Scripts/Gun.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] int layerToDamage;
+    [SerializeField] float damage = 10f;
+    [SerializeField] float range = 5;
+    [SerializeField] Camera FpsCamera;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = FpsCamera.transform.rotation;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(FpsCamera.transform.position, FpsCamera.transform.forward, out hit, range))
+        {
+            if(hit.transform.gameObject.layer == layerToDamage)
+            {
+                hit.transform.gameObject.GetComponent<IHitable>().OnHit(damage);
+            }
+        }
+    }
+}
