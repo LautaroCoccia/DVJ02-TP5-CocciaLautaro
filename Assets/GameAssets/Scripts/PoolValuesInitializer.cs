9c365ae5a30ab4c1ce0c8fef5e2cdae7b36088b9
LautaroCoccia <lautaroauditore@gmail.com>
2021-04-27 16:17:37 -0300

    Add PoolValueInitializer

---
+++ Assets/GameAssets/Scripts/PoolValuesInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolValuesInitializer : MonoBehaviour,IHitable, IPoolValuesInitializer
+{
+    [SerializeField] float health;
+    public void SetHealth(float _health)
+    {
+        health = _health;
+    }
+    public float GetHealth()
+    {
+        return health;
+    }
+    public void OnHit(float damage)
+    {
+        Debug.Log("Health: "+ health);
+        health -= damage;
+        Debug.Log("Health: " + health);
+
+        if (health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
