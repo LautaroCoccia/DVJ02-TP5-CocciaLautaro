fe64a9b33fd8d9dfc7ce4ca9570077dc37beb00f
LautaroCoccia <lautaroauditore@gmail.com>
2021-04-27 23:18:56 -0300

    Add Player.cs

---
+++ Assets/GameAssets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float health = 100;
+    public int score;
+    public void SetHealth(float _damage)
+    {
+        health -= _damage;
+    }
+    public float GetHealth()
+    {
+        return health;
+    }
+    public void SetScore(int _score)
+    {
+        score += _score;
+    }
+    public int GetScore()
+    {
+        return score;
+    }
+}
